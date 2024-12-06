e83d122959bf5bf9e3698f4d8a1b2e5184efb7b9
Santhosh Kumar <santhoshkumar.swaminathan@concertidc.com>
2024-12-06 13:10:01 +0530

    updated main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-  backend "s3" {}
-}
+# terraform {
+#   backend "s3" {}
+# }
 
 resource "aws_s3_bucket" "example" {
   bucket        = var.bucket_name
